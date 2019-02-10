21470a7b0c2915ee492bba1c7c17fe7491143967
nanxy <nancyzhou15@gmail.com>
2019-02-10 03:14:32 -0800

    implemented node.js capablilities, switched out html for handle bars

---
+++ public/js/proj.js
@@ -0,0 +1,15 @@
+//back button
+function goBack() {
+  window.history.back();
+}
+
+//login page
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
